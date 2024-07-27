8b7a9f92f2ba07e3c2638ad97c37b619ac9267cb
viniciuspiotto <viniciuspiotto.dev@gmail.com>
2024-07-27 10:30:18 -0300

    feat: add Product entity and its associations
    Add the Product class as a JPA entity, define relationships between Product and Category entites, and configure necessary mappings and annotations.

--- src/main/java/com/piotto/apiproduct/ApiProductApplication.java
+++ src/main/java/com/piotto/apiproduct/ApiProductApplication.java
@@ -1,13 +1,48 @@
 package com.piotto.apiproduct;
 
+import com.piotto.apiproduct.domain.Category;
+import com.piotto.apiproduct.domain.Product;
+import com.piotto.apiproduct.repositories.CategoryRepository;
+import com.piotto.apiproduct.repositories.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+import java.util.List;
+
 @SpringBootApplication
-public class ApiProductApplication {
+public class ApiProductApplication implements CommandLineRunner {
+
+    private final CategoryRepository categoryRepository;
+    private final ProductRepository productRepository;
+
+    public ApiProductApplication(CategoryRepository categoryRepository, ProductRepository productRepository) {
+        this.categoryRepository = categoryRepository;
+        this.productRepository = productRepository;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ApiProductApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Category cat1 = new Category(null, "Informatica");
+        Category cat2 = new Category(null, "Escritorio");
+
+        Product p1 = new Product(null, "Computer", 2000.0);
+        Product p2 = new Product(null, "Impressora", 800.0);
+        Product p3 = new Product(null, "Mouse", 80.0);
+
+        cat1.getProducts().addAll(Arrays.asList(p1, p2, p3));
+        cat2.getProducts().add(p2);
+
+        p1.getCategories().add(cat1);
+        p2.getCategories().addAll(Arrays.asList(cat1, cat2));
+        p3.getCategories().add(cat1);
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+        productRepository.saveAll(Arrays.asList(p1, p2, p3));
+    }
 }
